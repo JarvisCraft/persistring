644a9861459067175bc3674e7ff658f7d2db2175
Petr Portnov <gh@progrm-jarvis.ru>
2021-11-30 17:22:00 +0300

    feat: add `PersistentString`

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+/// A string providing persistent operations.
+trait PersistentString {
+    // State-checking operations
+
+    fn len(&self) -> usize;
+
+    // Copying operations
+
+    fn snapshot(&self) -> String;
+
+    // Mutating operations
+
+    fn push_str(&mut self, string: &str);
+
+    // Persistence management operations
+
+    fn undo(&mut self) -> Result<(), UndoError>;
+
+    fn redo(&mut self) -> Result<(), UndoError>;
+}
+
+/// An error which may happen when undoing an operation.
+enum UndoError {
+    Terminal,
+}
+
+/// An error which may occur when redoing an operation.
+enum RedoError {
+    Terminal,
 }
